6f554ecc324348f265c94b38e871ad38c66eacd4
Yoav Ram <yoav@yoavram.com>
2017-03-28 18:10:09 +0300

    sge->sh

---
+++ cluster/polymorphism.sh
@@ -0,0 +1,10 @@
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B1 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B2 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B3 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B4 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B5 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B6 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B7 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B8 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B9 --output_folder polymorphism
+python src/random_simulation.py --Ne 100000 --n 1000  --π0 C0.01  --env A1B10 --output_folder polymorphism
