c2b954768a43fa932d24f7105f54c248003ac082
Yoav Ram <yoav@yoavram.com>
2017-09-28 12:09:32 +0300

    do real work

--- src/stable.py
+++ src/stable.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     w = float(sys.argv[1])
     k = int(sys.argv[2])
     l = k      
-    ρ = 0.5#evol_stable(w=w, k=k, l=k, reps=1, PRINT=True)
+    ρ = evol_stable(w=w, k=k, l=k, reps=1, PRINT=True)
     fname = 'stable_{}.json'.format(uuid().hex)
     with open(fname, 'wt') as f:
     	json.dump(
